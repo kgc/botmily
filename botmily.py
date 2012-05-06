0ace06e44c9dcde8f8853088ee617324f6c2de96
Emily <kiwighostchips@gmail.com>
2012-05-06 16:26:58 -0400

    Added config file processing and multiple channels.

--- botmily.py
+++ botmily.py
@@ -5,20 +5,36 @@ from __future__ import unicode_literals
 from twisted.internet import reactor, protocol
 from twisted.words.protocols import irc
 
+import config
+
 class Bot(irc.IRCClient):
+    def __init__(self, name, channels):
+        self.nickname = name
+        self.realname = b"Botmily https://github.com/kgc/botmily"
+        self.channels = channels
+
     def signedOn(self):
-        self.join(b"#botmily")
+        print("Signed on to the IRC server")
+        for channel in self.channels:
+            self.join(str(channel))
 
     def joined(self, channel):
-        self.say(b"#botmily", b"hello world")
+        print("Joined channel " + channel)
+        self.say(channel, b"hello world")
 
 class BotFactory(protocol.ClientFactory):
+    def __init__(self, name, channels):
+        self.name = name
+        self.channels = channels
+
     def buildProtocol(self, addr):
-        p = Bot()
+        p = Bot(self.name, self.channels)
         p.factory = self
         return p
 
 if __name__ == '__main__':
-    f = BotFactory()
-    reactor.connectTCP("irc.synirc.net", 6667, f)
+    print("Starting the bot")
+    name, server, channels = config.getConfig()
+    f = BotFactory(name, channels.split(" "))
+    reactor.connectTCP(server, 6667, f)
     reactor.run()
