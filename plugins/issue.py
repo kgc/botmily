33bed59b7d9ff9f916e2d1600b15852aa7b9f849
Emily <kiwighostchips@gmail.com>
2012-05-14 18:13:29 -0400

    Added simple issue tracking plugin.

---
+++ plugins/issue.py
@@ -0,0 +1,17 @@
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import re
+
+from botmily.db import db
+
+def hook(nick, message):
+    if re.match('.issue', message) is None:
+        return None
+
+    db.execute("create table if not exists issues(id integer primary key, issue)")
+    issue = message[7:]
+    db.execute("insert into issues(id, issue) values(null, :issue)", {"issue": issue})
+    db.commit()
+    return 'Thanks for your worthless complaint.'
