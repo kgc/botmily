cd3668c4729674a8c6e60ada7fd5fb576a0f908c
Emily <kiwighostchips@gmail.com>
2012-05-14 18:02:42 -0400

    Blippy only owns 10% of the time.

--- plugins/blippy.py
+++ plugins/blippy.py
@@ -2,9 +2,12 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import random
 import re
 
 def hook(nick, message):
     if re.search('blippy', message) is None:
         return None
-    return 'blippy owns'
+    if random.randint(0, 9) == 0:
+        return 'blippy owns'
+    return None
