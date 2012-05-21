86c69c48122dfafe11f5e2986bc387115a73a9b6
Sam <sam@sam-VirtualBox.(none)>
2012-05-21 15:58:08 +0100

    Added a bunch of achievements

--- plugins/achievments.py
+++ plugins/achievments.py
@@ -7,14 +7,21 @@ from botmily.db import db
 import achievChecks
 
 def checkAchievs(nick , host , message):
-    results = ''
+    results = None
     for x in dir(achievChecks):
         if x.find('check') != -1:
             result = getattr(achievChecks,x)(nick,host,message)
             if result:
-                results = results + ' ' + result
-    return results
-   
+                if len(results) == 0:
+                    results = result
+                else:
+                    results = results + ' , ' + result
+    if results:
+        blurb = '~Congratulations %s you unlocked ' %nick
+        return blurb + results + '~'
+    else:
+        return None
+
 def hook(nick, ident, host, message):
     db.execute("create table if not exists achiv(aid integer , host text , value integer)")
     result = checkAchievs(nick , host, message)
