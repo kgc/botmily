703968a53d4858fef9876c8efde9480dfbfb1882
Sam <sam@sam-VirtualBox.(none)>
2012-05-30 23:30:44 +0100

    new regex for youtube stuff , l video length fix , less verbose on date posted

--- plugins/youtube.py
+++ plugins/youtube.py
@@ -5,10 +5,22 @@ from __future__ import unicode_literals
 import locale
 import re
 import time
-
+from datetime import datetime, timedelta
 from gdata.youtube import service
 
-regex = r'(?:http://www.youtube.com/watch\?v=)([-_a-z0-9]+)'
+regex = r'(?:youtube.*?(?:v=|/v/)|youtu\.be/|yooouuutuuube.*?id=)([-_a-z0-9]+)'
+def convertHMS(secs):
+    sec = timedelta(seconds=int(secs))
+    d = datetime(1,1,1) + sec
+    if d.day-1 > 0:
+        return '%d days %d hours %d minutes %d seconds' %(d.day-1,d.hour,d.minute,d.second)
+    elif d.hour > 0:
+        return '%d hours %d minutes %d seconds' %(d.hour,d.minute,d.second)
+    elif d.minute > 0:
+        return '%d minutes %d seconds' %(d.minute,d.second)
+    else:   
+        return '%d seconds' %d.second 
+  
 
 def hook(nick, ident, host, message):
     video_uri = re.search(regex, message, re.I)
@@ -20,10 +32,11 @@ def hook(nick, ident, host, message):
     entry = youtube.GetYouTubeVideoEntry(video_id=id)
     string = "\u0002"
     string += entry.media.title.text + "\u000f - length \u0002"
-    string += entry.media.duration.seconds + "\u000f - rated \u0002"
+    string += convertHMS(entry.media.duration.seconds) + "\u000f - rated \u0002"
     string += locale.format("%.2f", float(entry.rating.average)) + "/5.0\u000f ("
     string += entry.rating.num_raters + ") - \u0002"
     string += locale.format("%d", float(entry.statistics.view_count), True) + "\u000f views - \u0002"
     string += entry.author[0].name.text + "\u000f on \u0002"
-    string += time.strftime("%Y.%m.%d %H:%M:%S", time.strptime(entry.published.text, "%Y-%m-%dT%H:%M:%S.000Z"))
-    return string
+    string += time.strftime("%Y.%m.%d", time.strptime(entry.published.text, "%Y-%m-%dT%H:%M:%S.000Z"))
+    print(string)
+    return None
