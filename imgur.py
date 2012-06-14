01d5b1420942becc369fe06f14f192fcfb74edd6
samvm <samvm@samvm-VirtualBox.(none)>
2012-06-14 16:16:23 -0500

    imgur

---
+++ imgur.py
@@ -0,0 +1,18 @@
+from StringIO import StringIO
+import pycurl
+def postToImgur(filename):
+    store = StringIO()
+    c = pycurl.Curl()
+    values = [
+              ("key", "a95e5a7d90d1f821714e449bb47e1051"),
+              ("image", (c.FORM_FILE, filename))]
+    c.setopt(c.URL, "http://api.imgur.com/2/upload.xml")
+    c.setopt(c.HTTPPOST, values)
+    c.setopt(c.WRITEFUNCTION,store.write)
+    c.perform()
+    c.close()
+    retval = store.getvalue()
+    originalindex = retval.rfind('<original>')
+    endindex = retval.find('</original>')
+    url = retval[originalindex+10:endindex]
+    return url
