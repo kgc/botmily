f5735a7ce695ae79ff448980db9aabdbf0232ebb
samvm <samvm@samvm-VirtualBox.(none)>
2012-07-12 00:14:27 -0500

    poop

--- plugins/youtube.py
+++ plugins/youtube.py
@@ -22,8 +22,7 @@ def convertHMS(secs):
         return '%d seconds' %d.second 
   
 
-<<<<<<< HEAD
-def hook(nick, ident, host, message):
+def hook(nick, ident, host, message, bot, channel):
     if re.match('.yt ', message):
         search = message[4:]
         yt_service = service.YouTubeService()
@@ -35,24 +34,21 @@ def hook(nick, ident, host, message):
         title = feed.entry[0].title.text
         link = feed.entry[0].link[0].href
         return "\u0002%s\u000f - %s" %(title , link)
+    else:
+        video_uri = re.search(regex, message, re.I)
+        if video_uri is None:
+            return None
 
-=======
-def hook(nick, ident, host, message, bot, channel):
->>>>>>> 1712dabffc5bb8d51a6c830e57a1addd754a4746
-    video_uri = re.search(regex, message, re.I)
-    if video_uri is None:
-        return None
-
-    id = video_uri.group(1)
-    youtube = service.YouTubeService()
-    youtube.ssl = True
-    entry = youtube.GetYouTubeVideoEntry(video_id=id)
-    string = "\u0002"
-    string += entry.media.title.text + "\u000f - length \u0002"
-    string += convertHMS(entry.media.duration.seconds) + "\u000f - rated \u0002"
-    string += locale.format("%.2f", float(entry.rating.average)) + "/5.0\u000f ("
-    string += entry.rating.num_raters + ") - \u0002"
-    string += locale.format("%d", float(entry.statistics.view_count), True) + "\u000f views - \u0002"
-    string += entry.author[0].name.text + "\u000f on \u0002"
-    string += time.strftime("%Y.%m.%d", time.strptime(entry.published.text, "%Y-%m-%dT%H:%M:%S.000Z"))
-    return string
+        id = video_uri.group(1)
+        youtube = service.YouTubeService()
+        youtube.ssl = True
+        entry = youtube.GetYouTubeVideoEntry(video_id=id)
+        string = "\u0002"
+        string += entry.media.title.text + "\u000f - length \u0002"
+        string += convertHMS(entry.media.duration.seconds) + "\u000f - rated \u0002"
+        string += locale.format("%.2f", float(entry.rating.average)) + "/5.0\u000f ("
+        string += entry.rating.num_raters + ") - \u0002"
+        string += locale.format("%d", float(entry.statistics.view_count), True) + "\u000f views - \u0002"
+        string += entry.author[0].name.text + "\u000f on \u0002"
+        string += time.strftime("%Y.%m.%d", time.strptime(entry.published.text, "%Y-%m-%dT%H:%M:%S.000Z"))
+        return string
