3acfcfb887f777cb4b53eceaedaedf3c3c834f91
Emily <kiwighostchips@gmail.com>
2012-08-14 18:11:11 -0400

    Added catte plugin.

---
+++ plugins/catte.py
@@ -0,0 +1,15 @@
+﻿# -*- coding: utf-8 -*-
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import re
+from urllib2 import urlopen
+
+def hook(nick, ident, host, message, bot, channel):
+    if re.match('\.catte', message) is None:
+        return
+
+    result = urlopen('http://cattes.me:3333/random')
+    return 'http://cattes.me:3333/images/' + result.read()
