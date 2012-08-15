2013b3729a47e4bc202c46a3bc700b5540591151
Emily <kiwighostchips@gmail.com>
2012-08-15 18:36:47 -0400

    Made facebook plugin use random result.

--- plugins/facebook.py
+++ plugins/facebook.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import json
+import random
 import re
 from urllib import quote_plus
 from urllib2 import urlopen
@@ -12,10 +13,13 @@ from urllib2 import urlopen
 def facebook(message_data, bot):
     result = urlopen('http://graph.facebook.com/search?q=' + quote_plus(message_data["parsed"]))
     json_data = json.load(result)
+    usable_results = []
     for item in json_data['data']:
         if 'message' in item:
-            return item['from']['name'] + ': ' + item['message'].replace('\n', ' ')[:200] + ' Profile Link: http://facebook.com/profile.php?id=' + item['id'] + '&v=wall'
+            usable_results.append(item)
+    item = random.choice(usable_results)
+    return item['from']['name'] + ': ' + item['message'].replace('\n', ' ')[:200] + ' Profile Link: http://facebook.com/profile.php?id=' + item['id'] + '&v=wall'
 
-commands = {"facebook": facebook}
+commands = {"facebook": facebook, "ope": facebook}
 triggers = []
 
