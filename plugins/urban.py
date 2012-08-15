c65bd930ed69b1258ccf9590f447c03953dd490c
Emily <kiwighostchips@gmail.com>
2012-08-14 20:21:17 -0400

    Added urban dictionary plugin.

---
+++ plugins/urban.py
@@ -0,0 +1,19 @@
+﻿# -*- coding: utf-8 -*-
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import re
+from urllib2 import urlopen
+
+from BeautifulSoup import BeautifulStoneSoup
+
+def hook(nick, ident, host, message, bot, channel):
+    if re.match('\.u', message) is None:
+        return
+
+    result = urlopen('http://www.urbandictionary.com/define.php?term=' + message[3:])
+    soup = BeautifulStoneSoup(result, convertEntities=BeautifulStoneSoup.HTML_ENTITIES)
+    definition = soup.find('div', attrs={'class': 'definition'})
+    return definition.text
