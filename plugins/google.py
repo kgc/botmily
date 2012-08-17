5ed98d9b187bea05b275799d62a996c3760f4606
Emily <kiwighostchips@gmail.com>
2012-08-17 13:54:49 -0400

    Fixes for google plugin.

--- plugins/google.py
+++ plugins/google.py
@@ -15,8 +15,10 @@ from BeautifulSoup import BeautifulStoneSoup
 from botmily import irc
 
 def google(message_data, bot):
-    result = urlopen('http://ajax.googleapis.com/ajax/services/search/web?v=1.0&safe=off&q=' + quote_plus(message_data["parsed"]))
+    result = urlopen('http://ajax.googleapis.com/ajax/services/search/web?v=1.0&safe=off&q=' + quote_plus(message_data["parsed"].encode("utf-8")))
     json_data = json.load(result)
+    if len(json_data["responseData"]["results"]) == 0:
+        return "No results found"
     first_result = json_data['responseData']['results'][0]
     output = first_result['unescapedUrl'] + ' | '
     output += irc.bold(BeautifulStoneSoup(first_result['titleNoFormatting'], convertEntities=BeautifulStoneSoup.HTML_ENTITIES).text) + ' | '
@@ -24,8 +26,10 @@ def google(message_data, bot):
     return output
 
 def gis(message_data, bot):
-    result = urlopen('http://ajax.googleapis.com/ajax/services/search/images?v=1.0&safe=off&q=' + quote_plus(message_data["parsed"]))
+    result = urlopen('http://ajax.googleapis.com/ajax/services/search/images?v=1.0&safe=off&q=' + quote_plus(message_data["parsed"].encode("utf-8")))
     json_data = json.load(result)
+    if len(json_data["responseData"]["results"]) == 0:
+        return "No results found"
     chosen_result = random.choice(json_data["responseData"]["results"][:10])
     return chosen_result["unescapedUrl"]
 
