af196285e3b6bc03d9065982b2504caa3c04cccb
Emily <kiwighostchips@gmail.com>
2013-01-14 22:25:37 -0500

    Randomness fix for quote plugin.

--- plugins/quote.py
+++ plugins/quote.py
@@ -4,7 +4,6 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import random
 import time
 
 from botmily.db import db
@@ -16,10 +15,10 @@ def quote(message_data, bot):
         db.commit()
         return "Quote added."
     elif message_data["parsed"][:7] == "search ":
-        quotes = db.execute("select sender, quote, time from quote where quote like :quote", {"quote": "%" + message_data["parsed"][7:] + "%"}).fetchall()
+        quotes = db.execute("select sender, quote, time from quote where quote like :quote order by random()", {"quote": "%" + message_data["parsed"][7:] + "%"}).fetchall()
         if len(quotes) == 0:
             return "Nothing found."
-        row = random.choice(quotes)
+        row = quotes[0]
         return "Found " + str(len(quotes)) + " quotes: (" + row[0] + ") " + row[1]
     else:
         return "Unknown command."
