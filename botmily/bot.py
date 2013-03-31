1be50d1304275ee07531bd77f97a58148f3950f7
Emily <kiwighostchips@gmail.com>
2013-03-31 16:58:40 -0400

    Only print join message when the bot joins.

--- botmily/bot.py
+++ botmily/bot.py
@@ -13,71 +13,72 @@ from botmily import irc
 import plugins
 
 class bot():
-    def __init__(self):
-        self.server = config.server
-        self.nickname = config.name
-        self.realname = b"Botmily https://github.com/kgc/botmily"
-        self.channels = config.channels
-        self.password = config.password
+	def __init__(self):
+		self.server = config.server
+		self.nickname = config.name
+		self.realname = b"Botmily https://github.com/kgc/botmily"
+		self.channels = config.channels
+		self.password = config.password
 
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.connect((self.server, 6667))
-        self.irc = irc.irc_handler(self.socket, self)
+		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.socket.connect((self.server, 6667))
+		self.irc = irc.irc_handler(self.socket, self)
 
-        print("Initializing plugins...")
-        self.commands = {}
-        self.triggers = []
-        for importer, modname, ispkg in pkgutil.iter_modules(plugins.__path__):
-            print("Loading plugin " + modname)
-            plugin = __import__("plugins." + modname, fromlist="hook")
-            self.commands.update(plugin.commands)
-            self.triggers.extend(plugin.triggers)
+		print("Initializing plugins...")
+		self.commands = {}
+		self.triggers = []
+		for importer, modname, ispkg in pkgutil.iter_modules(plugins.__path__):
+			print("Loading plugin " + modname)
+			plugin = __import__("plugins." + modname, fromlist="hook")
+			self.commands.update(plugin.commands)
+			self.triggers.extend(plugin.triggers)
 
-        asyncore.loop()
+		asyncore.loop()
 
-    def join(self, channel):
-        print("Joined channel " + channel)
+	def join(self, nick, user, host, channel):
+		if nick == self.nickname:
+			print("Joined channel " + channel)
 
-    def privmsg(self, nick, user, host, channel, message):
-        message_data = {"nick":    nick,
-                        "user":    user,
-                        "host":    host,
-                        "channel": channel,
-                        "message": message}
-        if message_data["nick"] == "tarageddon":
-            return
-        command_match = re.match("\.([^ ]+) ?(.*)", message_data["message"])
-        if command_match is not None:
-            sent_command = command_match.group(1)
-            message_data["parsed"] = command_match.group(2)
-            possible_commands = []
-            for command, function in self.commands.iteritems():
-                if sent_command == command:
-                    possible_commands = [(command, function)]
-                    break
-                if command.find(sent_command) == 0:
-                    possible_commands.append((command, function))
-            if len(possible_commands) == 1:
-                message_data["command"] = possible_commands[0][0]
-                output = possible_commands[0][1](message_data, self)
-                self.say(nick, channel, output)
-            if len(possible_commands) > 1:
-                commands_formatted = []
-                for command, function in possible_commands:
-                    commands_formatted.append("." + command)
-                self.say(nick, channel, "Did you mean: " + ",".join(commands_formatted) + "?")
-        for tup in self.triggers:
-            trigger, function = tup
-            if re.search(trigger, message_data["message"], re.I) is not None:
-                message_data["re"] = re.search(trigger, message_data["message"], re.I)
-                output = function(message_data, self)
-                self.say(nick, channel, output)
+	def privmsg(self, nick, user, host, channel, message):
+		message_data = {"nick":    nick,
+		                "user":    user,
+		                "host":    host,
+		                "channel": channel,
+		                "message": message}
+		command_match = re.match("\.([^ ]+) ?(.*)", message_data["message"])
+		if command_match is not None:
+			sent_command = command_match.group(1)
+			message_data["parsed"] = command_match.group(2)
+			possible_commands = []
+			for command, function in self.commands.iteritems():
+				if sent_command == command:
+					possible_commands = [(command, function)]
+					break
+				if command.find(sent_command) == 0:
+					possible_commands.append((command, function))
+			if len(possible_commands) == 1:
+				message_data["command"] = possible_commands[0][0]
+				output = possible_commands[0][1](message_data, self)
+				self.say(nick, channel, output)
+			if len(possible_commands) > 1:
+				commands_formatted = []
+				for command, function in possible_commands:
+					commands_formatted.append("." + command)
+				self.say(nick, channel, "Did you mean: " +
+				                        ",".join(commands_formatted) + "?")
+		for tup in self.triggers:
+			trigger, function = tup
+			if re.search(trigger, message_data["message"], re.I) is not None:
+				message_data["re"] = re.search(trigger,
+				                               message_data["message"], re.I)
+				output = function(message_data, self)
+				self.say(nick, channel, output)
 
-    def say(self, nick, channel, output):
-        if output is None:
-            return
-        if self.nickname == channel:
-            self.irc.privmsg(nick, output)
-        else:
-            self.irc.privmsg(channel, nick + ": " + output)
+	def say(self, nick, channel, output):
+		if output is None:
+			return
+		if self.nickname == channel:
+			self.irc.privmsg(nick, output)
+		else:
+			self.irc.privmsg(channel, nick + ": " + output)
 
--- botmily/irc.py
+++ botmily/irc.py
@@ -10,107 +10,109 @@ controls = {'bold':  '\u0002',
             'color': '\u0003',
             'clear': '\u000f'}
 
-colors = {'white':        '00',
-          'black':        '01',
-          'blue':         '02',
-          'green':        '03',
-          'red':          '04',
-          'brown':        '05',
-          'purple':       '06',
-          'orange':       '07',
-          'yellow':       '08',
-          'light green':  '09',
-          'teal':         '10',
-          'light cyan':   '11',
-          'light blue':   '12',
-          'pink':         '13',
-          'grey':         '14',
-          'light grey':   '15'}
+colors = {	'white':       '00',
+		'black':       '01',
+		'blue':        '02',
+		'green':       '03',
+		'red':         '04',
+		'brown':       '05',
+		'purple':      '06',
+		'orange':      '07',
+		'yellow':      '08',
+		'light green': '09',
+		'teal':        '10',
+		'light cyan':  '11',
+		'light blue':  '12',
+		'pink':        '13',
+		'grey':        '14',
+		'light grey':  '15'}
 
 def clear(message):
-    new_string = ''
-    for character in message:
-        if unicodedata.category(character) != 'Cc':
-            new_string += character
-    return new_string
+	new_string = ''
+	for character in message:
+		if unicodedata.category(character) != 'Cc':
+			new_string += character
+	return new_string
 
 def bold(message):
-    return controls['bold'] + message + controls['clear']
+	return controls['bold'] + message + controls['clear']
 
 def color(message, color):
-    return controls['color'] + colors[color] + message + controls['clear']
+	return controls['color'] + colors[color] + message + controls['clear']
 
 def split_prefix(prefix):
-    nick, remainder = prefix[1:].split(b"!", 1)
-    user, host = remainder.split(b"@", 1)
-    return nick, user, host
+	nick, remainder = prefix[1:].split(b"!", 1)
+	user, host = remainder.split(b"@", 1)
+	return nick, user, host
 
 class irc_handler(asynchat.async_chat):
-    def __init__(self, sock, bot):
-        asynchat.async_chat.__init__(self, sock=sock)
-        self.ibuffer = b""
-        self.set_terminator(b"\r\n")
-        self.bot = bot
-        self.push(b"NICK %s\r\n" %bot.nickname)
-        self.push(b"USER botmily 0 0 :Botmily\r\n")
+	def __init__(self, sock, bot):
+		asynchat.async_chat.__init__(self, sock=sock)
+		self.ibuffer = b""
+		self.set_terminator(b"\r\n")
+		self.bot = bot
+		self.push(b"NICK %s\r\n" %bot.nickname)
+		self.push(b"USER botmily 0 0 :Botmily\r\n")
 
-    def collect_incoming_data(self, data):
-        self.ibuffer += data
+	def collect_incoming_data(self, data):
+		self.ibuffer += data
 
-    def found_terminator(self):
-        current_line = self.ibuffer
-        self.ibuffer = b""
-        prefix = None
-        if current_line[0] == b":":
-            prefix, null, current_line = current_line.partition(b" ")
-        command, null, params = current_line.partition(b" ")
-        if b":" in params:
-            other_params, null, trailing = params.partition(b":")
-            if len(other_params) == 0:
-                params = [trailing]
-            else:
-                params = other_params.split(" ") + [trailing]
-        else:
-            params = params.split(" ")
-        if command[0] >= b"0" and command[0] <= b"9":
-            if command == b"001":
-                self.privmsg("nickserv", "identify " + self.bot.password)
-                for channel in self.bot.channels:
-                    self.join(str(channel))
-        else:
-            try:
-                handler = getattr(self, "raw_" + command)
-            except AttributeError:
-                return
-            handler(prefix, params)
+	def found_terminator(self):
+		current_line = self.ibuffer
+		self.ibuffer = b""
+		prefix = None
+		if current_line[0] == b":":
+			prefix, null, current_line = current_line.partition(b" ")
+		command, null, params = current_line.partition(b" ")
+		if b":" in params:
+			other_params, null, trailing = params.partition(b":")
+			if len(other_params) == 0:
+				params = [trailing]
+			else:
+				params = other_params.split(" ") + [trailing]
+		else:
+			params = params.split(" ")
+		if command[0] >= b"0" and command[0] <= b"9":
+			if command == b"001":
+				self.privmsg("nickserv", "identify " + self.bot.password)
+				for channel in self.bot.channels:
+					self.join(str(channel))
+		else:
+			try:
+				handler = getattr(self, "raw_" + command)
+			except AttributeError:
+				return
+			handler(prefix, params)
 
-    def join(self, channel):
-        self.push(b"JOIN " + channel + b"\r\n")
+	def join(self, channel):
+		self.push(b"JOIN " + channel + b"\r\n")
 
-    def part(self, channel):
-        self.push(b"PART " + channel + b"\r\n")
+	def part(self, channel):
+		self.push(b"PART " + channel + b"\r\n")
 
-    def kick(self, channel, user):
-        self.push(b"KICK " + str(channel) + b" " + str(user) + b"\r\n")
+	def kick(self, channel, user):
+		self.push(b"KICK " + str(channel) + b" " + str(user) + b"\r\n")
 
-    def privmsg(self, receiver, message):
-        self.push(b"PRIVMSG " + str(receiver) + b" :" + message.encode("utf-8") + b"\r\n")
+	def privmsg(self, receiver, message):
+		self.push(b"PRIVMSG " + str(receiver) + b" :" + message.encode("utf-8") + b"\r\n")
 
-    def notice(self, receiver, message):
-        self.push(b"NOTICE " + str(receiver) + b" :" + message.encode("utf-8") + b"\r\n")
+	def notice(self, receiver, message):
+		self.push(b"NOTICE " + str(receiver) + b" :" + message.encode("utf-8") + b"\r\n")
 
-    def pong(self, message):
-        self.push(b"PONG " + message + b"\r\n")
+	def pong(self, message):
+		self.push(b"PONG " + message + b"\r\n")
 
-    def raw_PRIVMSG(self, prefix, params):
-        nick, user, host = split_prefix(prefix)
-        channel = params[0]
-        message = params[-1].decode("utf-8", "replace")
-        self.bot.privmsg(nick, user, host, channel, message)
+	def raw_PRIVMSG(self, prefix, params):
+		nick, user, host = split_prefix(prefix)
+		channel = params[0]
+		message = params[-1].decode("utf-8", "replace")
+		self.bot.privmsg(nick, user, host, channel, message)
 
-    def raw_PING(self, prefix, params):
-        self.pong(params[0])
+	def raw_PING(self, prefix, params):
+		self.pong(params[0])
 
-    def raw_JOIN(self, prefix, params):
-        self.bot.join(params[0])
+	def raw_JOIN(self, prefix, params):
+		nick, user, host = split_prefix(prefix)
+		channel = params[0]
+		self.bot.join(nick, user, host, channel)
 
