6087d38cdc50fe6c83ad64786ccfc9f7ea49dbbd
Emily <kiwighostchips@gmail.com>
2013-04-02 00:21:01 -0400

    Fix for youtube plugin.

--- plugins/youtube.py
+++ plugins/youtube.py
@@ -13,46 +13,48 @@ from botmily import irc
 regex = r'(?:youtube.*?(?:v=|/v/)|youtu\.be/|yooouuutuuube.*?id=)([-_a-z0-9]+)'
 
 def convertHMS(secs):
-    sec = timedelta(seconds=int(secs))
-    d = datetime(1,1,1) + sec
-    if d.day-1 > 0:
-        return '%d days %d hours %d minutes %d seconds' %(d.day-1,d.hour,d.minute,d.second)
-    elif d.hour > 0:
-        return '%d hours %d minutes %d seconds' %(d.hour,d.minute,d.second)
-    elif d.minute > 0:
-        return '%d minutes %d seconds' %(d.minute,d.second)
-    else:   
-        return '%d seconds' %d.second 
+	sec = timedelta(seconds=int(secs))
+	d = datetime(1,1,1) + sec
+	if d.day-1 > 0:
+		return '%d days %d hours %d minutes %d seconds' %(d.day-1,d.hour,d.minute,d.second)
+	elif d.hour > 0:
+		return '%d hours %d minutes %d seconds' %(d.hour,d.minute,d.second)
+	elif d.minute > 0:
+		return '%d minutes %d seconds' %(d.minute,d.second)
+	else:	
+		return '%d seconds' %d.second 
   
 def search(message_data, bot):
-    yt_service = service.YouTubeService()
-    query = service.YouTubeVideoQuery()
-    query.vq = message_data["parsed"]
-    query.orderby = 'relevance'
-    query.racy = 'include'
-    feed = yt_service.YouTubeQuery(query)
-    title = feed.entry[0].title.text
-    link = feed.entry[0].link[0].href
-    return "\u0002%s\u000f - %s" %(unicode(title, encoding='utf-8') , link)
+	yt_service = service.YouTubeService()
+	query = service.YouTubeVideoQuery()
+	query.vq = message_data["parsed"]
+	query.orderby = 'relevance'
+	query.racy = 'include'
+	feed = yt_service.YouTubeQuery(query)
+	if len(feed.entry) == 0:
+		return "No results"
+	title = feed.entry[0].title.text
+	link = feed.entry[0].link[0].href
+	return "\u0002%s\u000f - %s" %(unicode(title, encoding='utf-8') , link)
 
 def parse(message_data, bot):
-    id = message_data["re"].group(1)
-    youtube = service.YouTubeService()
-    youtube.ssl = True
-    try:
-        entry = youtube.GetYouTubeVideoEntry(video_id=id)
-    except service.RequestError:
-        return None
-    string = irc.bold(unicode(entry.media.title.text, encoding='utf-8')) + " - length "
-    string += irc.bold(convertHMS(entry.media.duration.seconds)) + " - rated "
-    if entry.rating is not None:
-        string += irc.bold(locale.format("%.2f", float(entry.rating.average))) + "/5.0 ("
-        string += entry.rating.num_raters + ") - "
-    if entry.statistics is not None:
-        string += irc.bold(locale.format("%d", float(entry.statistics.view_count), True)) + " views - "
-    string += irc.bold(unicode(entry.author[0].name.text, encoding='utf-8')) + " on "
-    string += irc.bold(time.strftime("%Y.%m.%d", time.strptime(entry.published.text, "%Y-%m-%dT%H:%M:%S.000Z")))
-    return string
+	id = message_data["re"].group(1)
+	youtube = service.YouTubeService()
+	youtube.ssl = True
+	try:
+		entry = youtube.GetYouTubeVideoEntry(video_id=id)
+	except service.RequestError:
+		return None
+	string = irc.bold(unicode(entry.media.title.text, encoding='utf-8')) + " - length "
+	string += irc.bold(convertHMS(entry.media.duration.seconds)) + " - rated "
+	if entry.rating is not None:
+		string += irc.bold(locale.format("%.2f", float(entry.rating.average))) + "/5.0 ("
+		string += entry.rating.num_raters + ") - "
+	if entry.statistics is not None:
+		string += irc.bold(locale.format("%d", float(entry.statistics.view_count), True)) + " views - "
+	string += irc.bold(unicode(entry.author[0].name.text, encoding='utf-8')) + " on "
+	string += irc.bold(time.strftime("%Y.%m.%d", time.strptime(entry.published.text, "%Y-%m-%dT%H:%M:%S.000Z")))
+	return string
 
 commands = {"yt": search, "youtube": search}
 triggers = [(regex, parse)]
