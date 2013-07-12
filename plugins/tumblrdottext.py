42ec5a480b33836ea950176165c6b3a232040638
Chris Zhang <cjzhang@berkeley.edu>
2013-07-11 21:30:28 -0700

    Update tumblrdottext to work with api v 1.1 (need to make an app at https://dev.twitter.com)

--- plugins/tumblrdottext.py
+++ plugins/tumblrdottext.py
@@ -3,12 +3,16 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 from twitter import Twitter
+from twitter import OAuth
+from botmily import config
 import random
-#randomly return one of the last 50 tweets from https://twitter.com/TumblrTXT
+#randomly return one of the last 200 tweets from https://twitter.com/TumblrTXT
+#rate limit is 300 requests per 15 minutes
 def tumblr(message_data, bot):
-    t = Twitter(api_version=1)
-    tweetno = random.randint(0,49)
-    tweet = t.statuses.user_timeline.tumblrtxt(count=50)[tweetno]
+    t = Twitter(api_version=1.1, auth=OAuth(config.oauth_token,
+    config.oauth_secret, config.consumer_key, config.consumer_secret))
+    tweetno = random.randint(0,199)
+    tweet = t.statuses.user_timeline.tumblrtxt(count=200)[tweetno]
     return tweet['text']
 
 
